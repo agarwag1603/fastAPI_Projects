c77868caeafcc71db544a32cf7faa274d262b87a
Gaurav agarwal <gaurav.a201992@gmail.com>
2025-11-14 14:18:53 +0800

    added delete riute

--- fastapi_project/fastapi_testing.py
+++ fastapi_project/fastapi_testing.py
@@ -116,3 +116,17 @@ def update_student(student_id: str, student_update:UpdateStudent):
     save_data(data)
     return JSONResponse(status_code=200, content={"message":"student updated"})
 
+
+@app.delete('/delete/{student_id}')
+def delete_student(student_id: str = Path(...,description="Get individual student records", example = "IT001")):
+
+    data= load_data()
+
+    if student_id not in data:
+        raise HTTPException(status_code=404, detail="Student record not found")
+
+    del data[student_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200,content={"message":"Student Deleted"})
